e8de8c4e0e7997aed5ff5a697a7b0a1072fa754a
DongHeon Lee <niche3600@gmail.com>
2022-03-26 07:55:09 +0900

    str example and generate bingo sheet example

---
+++ Sample_4_2_example/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Sample_4_2_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("英文を入力してください: ");
+            string str = Console.ReadLine();
+            Console.WriteLine($"大文字：{str.ToUpper()}");
+            Console.WriteLine($"小文字：{str.ToLower()}");
+        }
+    }
+}
---
+++ Sample_4_3_example/Bingo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace Sample_4_3_example
+{
+	public class Bingo
+	{
+		private int[,] data;
+		private int bingoSheetSize;
+
+		private void InitBingoSheet(int size)
+		{
+			data = new int[size, size];
+			bingoSheetSize = size * size;
+			for (int i = 0; i < data.GetLength(0); i++)
+			{
+				for (int j = 0; j < data.GetLength(1); j++)
+				{
+					data[i, j] = 0;
+				}
+			}
+
+			return;
+		}
+
+		private void GenerateBingo(int size)
+		{
+			Random rnd = new Random();
+
+			int num = 1;
+
+			while (num <= bingoSheetSize)
+			{
+
+				while (true)
+				{
+					int m = rnd.Next(size);
+					int n = rnd.Next(size);
+
+					if (data[m, n] == 0)
+					{
+						data[m, n] = num;
+						num++;
+						break;
+					}
+				}
+			}
+
+			return;
+		}
+
+		public void Init(int size)
+		{
+			InitBingoSheet(size);
+			GenerateBingo(size);
+		}
+
+		public void Show()
+		{
+			for (int i = 0; i < data.GetLength(0); i++)
+			{
+				for (int j = 0; j < data.GetLength(1); j++)
+				{
+					Console.Write("{0,2:d}|", data[i, j]);
+				}
+				Console.WriteLine();
+				for (int j = 0; j < data.GetLength(1); j++)
+				{
+					Console.Write("---");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
+
