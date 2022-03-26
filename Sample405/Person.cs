b4b688c8058081e769cc69362f6ac57ba2cfb0fd
DongHeon Lee <niche3600@gmail.com>
2022-03-26 10:51:32 +0900

    auto-implemented properties example

---
+++ Sample405/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Sample405
+{
+	public class Person
+	{
+		public void SetAgeAndName(string name, int age)
+		{
+			Name = name;
+			Age = age;
+		}
+
+		public void ShowAgeAndName()
+		{
+			Console.WriteLine($"名前：{Name} 年齢：{Age}");
+		}
+
+		public string Name
+		{
+			private set; get;
+		}
+
+		public int Age
+		{
+			set; get;
+		}
+	}
+}
+
---
+++ Sample405/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sample405
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.SetAgeAndName("山田太郎", 26);
+            p.Age = 32;
+
+            // p.Name = 36;
+
+            Console.WriteLine($"名前：{p.Name} 年齢：{p.Age}");
+        }
+    }
+}
