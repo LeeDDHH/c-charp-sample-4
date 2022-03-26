c50ab0f41a337189b66870884382336f6198e3b5
DongHeon Lee <niche3600@gmail.com>
2022-03-26 10:19:33 +0900

    use private access modifiers and set, get property example

---
+++ Sample403/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Sample403
+{
+	public class Person
+	{
+		private string name = "";
+		private int age = 0;
+
+		public void SetAgeAndName(string name, int age)
+		{
+			this.name = name;
+			this.age = age;
+		}
+
+		public void ShowAgeAndName()
+		{
+			Console.WriteLine($"名前：{name} 年齢：{age}");
+		}
+
+		public string Name
+		{
+			set { name = value; }
+			get { return name; }
+		}
+
+		public int Age
+		{
+			set { age = value; }
+            get { return age; }
+		}
+	}
+}
+
---
+++ Sample403/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Sample403
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Person p1, p2;
+            p1 = new Person();
+            p2 = new Person();
+
+            p1.Name = "山田太郎";
+            p1.Age = 19;
+
+            p2.SetAgeAndName("佐藤花子", 23);
+
+            p1.ShowAgeAndName();
+            p2.ShowAgeAndName();
+        }
+    }
+}
