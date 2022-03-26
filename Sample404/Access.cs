7fb33ef9bfc3957e5fb10b6b6d31da9ba797db28
DongHeon Lee <niche3600@gmail.com>
2022-03-26 10:30:40 +0900

    use get set property example

---
+++ Sample404/Access.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Sample404
+{
+	public class Access
+	{
+		private int data1 = 5;
+		private int data2 = 0;
+
+		public void ShowData()
+		{
+			Console.WriteLine($"data1={data1} data2={data2}");
+		}
+
+		public int Data1
+		{
+			get { return data1; }
+		}
+
+		public int Data2
+		{
+			set { data2 = value; }
+		}
+	}
+}
+
---
+++ Sample404/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Sample404
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Access a = new Access();
+            // a.Data1 = 1;
+            a.Data2 = 2;
+            a.ShowData();
+
+            Console.WriteLine($"a.data1 = {a.Data1}");
+            // Console.WriteLine($"a.data2 = {a.Data2}");
+        }
+    }
+}
+
