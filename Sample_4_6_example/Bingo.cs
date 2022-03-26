34bc67f0eb5328a35e2bd10db15dd93e708f4a45
DongHeon Lee <niche3600@gmail.com>
2022-03-26 18:57:41 +0900

    add private method example

---
+++ Sample_4_6_example/Bingo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace Sample_4_3_example
+{
+	public class Bingo
+	{
+		private int[,] data;
+        private int size;
+        private int bingoSheetSize;
+
+        private void InitFiled(int size)
+        {
+            this.size = size;
+            data = new int[size, size];
+            bingoSheetSize = size * size;
+            return;
+        }
+
+        private void InitBingoSheet()
+		{
+			for (int i = 0; i < data.GetLength(0); i++)
+			{
+				for (int j = 0; j < data.GetLength(1); j++)
+				{
+					data[i, j] = 0;
+				}
+			}
+
+			return;
+		}
+
+		private void GenerateBingo()
+		{
+			Random rnd = new Random();
+
+			int num = 1;
+
+			while (num <= bingoSheetSize)
+			{
+
+				while (true)
+				{
+					int m = rnd.Next(size);
+					int n = rnd.Next(size);
+
+					if (data[m, n] == 0)
+					{
+						data[m, n] = num;
+						num++;
+						break;
+					}
+				}
+			}
+
+			return;
+		}
+
+		public void Init(int size)
+		{
+            InitFiled(size);
+            InitBingoSheet();
+			GenerateBingo();
+		}
+
+		public void Show()
+		{
+			for (int i = 0; i < data.GetLength(0); i++)
+			{
+				for (int j = 0; j < data.GetLength(1); j++)
+				{
+					Console.Write("{0,2:d}|", data[i, j]);
+				}
+				Console.WriteLine();
+				for (int j = 0; j < data.GetLength(1); j++)
+				{
+					Console.Write("---");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
---
+++ Sample_4_6_example/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Sample_4_3_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bingo sheet = new Bingo();
+            Console.Write("ビンゴのマスの縦・横のサイズを入力：");
+            int size = int.Parse(Console.ReadLine());
+            sheet.Init(size);
+            sheet.Show();
+        }
+    }
+}
