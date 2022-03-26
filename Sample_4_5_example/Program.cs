acc022944382eda27c96000f98957713f2d1828c
DongHeon Lee <niche3600@gmail.com>
2022-03-26 18:21:59 +0900

    get string length example

---
+++ Sample_4_5_example/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample_4_5_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字列を入力してください：");
+            string str = Console.ReadLine();
+            Console.WriteLine($"文字列の長さ：{str.Length}");
+        }
+    }
+}
