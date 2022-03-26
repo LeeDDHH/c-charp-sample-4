0f21c8315473c1726dd9e9dc757357ebecde7893
DongHeon Lee <niche3600@gmail.com>
2022-03-26 11:38:24 +0900

    use property example

---
+++ Sample_4_4_example/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sample_4_4_example
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+
+            Console.Write("幅（cm）: ");
+            r.Width = double.Parse(Console.ReadLine());
+
+            Console.Write("高さ（cm）: ");
+            r.Height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"面積：{r.GetArea()}cm2");
+            Console.WriteLine($"周の長さ：{r.GetPerimeter()}cm");
+        }
+    }
+}
---
+++ Sample_4_4_example/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Sample_4_4_example
+{
+	public class Rectangle
+	{
+		private double width;
+		private double height;
+
+		public double Width
+		{
+			set { width = value; }
+			get { return width; }
+		}
+
+		public double Height
+		{
+			set { height = value; }
+			get { return height; }
+		}
+
+    public double GetArea()
+    {
+      return width * height;
+    }
+
+    public double GetPerimeter()
+    {
+      return width * 2 + height * 2;
+    }
+
+  }
+}
